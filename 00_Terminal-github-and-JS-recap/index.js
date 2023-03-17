1b138310a31da63a697bd74dc3d76906a96b148f
Princeton J. Rose <princetonjrose@gmail.com>
2023-03-17 13:10:53 -0400

    changed folder name and added in JS recap file

---
+++ 00_Terminal-github-and-JS-recap/index.js
@@ -0,0 +1,74 @@
+// Start here!
+
+
+// 💡 JS data types:
+/*
+    string
+    number
+    float
+    boolean
+    array
+    object
+    null
+    undefined
+*/
+
+// ✅ Show each of the data types
+
+
+
+// 💡 Storing values in variables
+
+// ✅ Create a variable with let and store different data types in it
+
+
+
+// ✅ Create a variable with const and try to change the value
+
+
+
+// 💡 Conditionals - if / else if / else
+
+// ✅ Create a conditional statement
+
+
+
+// ✅ Create a conditional statement that shows the difference between truthy and falsy values
+
+
+
+// 💡 Arrays and array iteration
+
+// ✅ Create an array of numbers
+
+
+
+// ✅ Iterate through the array using a for loop
+
+
+
+// ✅ Iterate through the array using a forEach loop
+
+
+
+// ✅ Iterate through the array and add two to each number with map
+
+
+
+// ✅ Iterate through the array and filter only the numbers greater than 10
+
+// ❓ When do I use forEach vs map? 🤔
+
+// 💡 Working with Objects
+
+// ✅ Create a person object with the keys 'name' and 'role' and assign them values
+
+
+
+// ✅ Access the key/value pairs of the object using both dot notation and bracket notation
+
+
+
+// ✅ Change the values of 'name' and 'role'
+
+
